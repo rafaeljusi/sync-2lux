653e6e46013f9f13f255764aacb60a309afdc7fe
Rafael Jusi <rafael@jusi.com.br>
2023-03-12 11:23:36 -0300

    Corrects naming conventions

--- sync_2lux/__init__.py
+++ sync_2lux/__init__.py
@@ -5,8 +5,8 @@ import os
 import time
 import numpy as np
 import pandas as pd
-from bson.codec_options import CodecOptions, DEFAULT_CODEC_OPTIONS
-from bson.binary import Binary, UuidRepresentation
+from bson.codec_options import CodecOptions
+from bson.binary import UuidRepresentation
 from uuid import UUID
 from pymongo import MongoClient
 from pymongo import UpdateOne
@@ -20,7 +20,7 @@ def is_json(myjson):
 
 CODEC_OPTIONS = CodecOptions(uuid_representation=UuidRepresentation.STANDARD)
 
-def getDBClient():
+def get_dbclient():
     with open('config.json') as config_file:
         config = json.load(config_file)
 
@@ -37,7 +37,7 @@ def getDBClient():
         print("Database Server not available")
         quit()
 
-def getProducts():
+def get_products():
     with open('config_products.json') as config_file:
         config = json.load(config_file)
 
@@ -52,7 +52,7 @@ def getProducts():
 
     return dataset
 
-def writeRecordsToDB(dataset, collection):
+def write_records_to_db(dataset, collection):
     if(len(dataset) > 0):
 
         data_dict = dataset.to_dict("records")
@@ -71,15 +71,15 @@ def writeRecordsToDB(dataset, collection):
         if (len(operations) > 0):
             collection.bulk_write(operations,ordered=False)
 
-def saveState():
-    state = getState()
+def save_state():
+    state = get_state()
 
     state["last_sync"] = date.today().strftime('%Y-%m-%d')
 
     with open('state.json', 'w') as state_file:
             state_file.write(json.dumps(state))
 
-def getState():
+def get_state():
     state = {}
 
     if os.path.exists('state.json') and os.path.getsize('state.json') > 0:
@@ -90,27 +90,27 @@ def getState():
     return state
 
 def main():
-    state = getState()
+    state = get_state()
     
     if(state.get('last_sync') != None and state["last_sync"] >= date.today()):
         print(f'Last sync: {state["last_sync"]}. Sync skipped')
         quit()
 
-    db = getDBClient()
+    db = get_dbclient()
 
     start = time.time()
 
     print("Reading products from file...")
-    products = getProducts()
+    products = get_products()
 
     print("Writing products to DB...")
     collection= db.get_collection("products", CODEC_OPTIONS)
-    writeRecordsToDB(dataset=products, collection=collection)
+    write_records_to_db(dataset=products, collection=collection)
 
     end = time.time()
-    timeElapsed = end - start
-    recordCount = len(products)
+    time_elapsed = end - start
+    record_count = len(products)
 
-    saveState()
+    save_state()
 
-    print(f'Finished => records: {recordCount}. time elapsed: {timeElapsed:.0f}s. ~{recordCount / timeElapsed:.0f} records per second')
+    print(f'Finished => records: {record_count}. time elapsed: {time_elapsed:.0f}s. ~{record_count / time_elapsed:.0f} records per second')
